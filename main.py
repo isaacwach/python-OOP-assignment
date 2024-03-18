4f23b89e1b37b2189708d617c9cf2e715ccb71bc
isaacwach <isaacwachira57@gmail.com>
2024-03-18 14:50:43 +0300

    bulk commit

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ class Person:
         self.name = name
         self.age = age
         self.gender = gender
+
+    def introduce(self):
+        print(f"Meet {self.name} who is a {self.gender} and is {self.age} years old")
+
+person1 = Person("Zacs", 34, "Male")
+person1.introduce()
